8e6315af67ae87584c6e56d83b3b91cf54301059
404 not found <124446532+Yang-Zhikun@users.noreply.github.com>
2024-08-22 17:36:46 +0800

    更新 main.cpp

--- Tree/MinHeap/main.cpp
+++ Tree/MinHeap/main.cpp
@@ -1,8 +1,18 @@
 #include"MinHeap.h"
 #include<iostream>
 
-int main(){
-    MinHeap<int> minHeap;
-
-    system("pause");
-}
+int main() { 
+     int arr[200]; 
+     srand(time(0)); 
+     for(int i=0;i<=10;i++){ 
+         arr[i] = rand()%10; 
+         cout<<arr[i]<<' '; 
+     } 
+     cout<<'\n'; 
+     MinHeap<int> heap(arr, 11); 
+     heap.print(); 
+     while(!heap.isEmpty()){ 
+         cout<<heap.pop()<<endl; 
+     } 
+     system("pause"); 
+ }
